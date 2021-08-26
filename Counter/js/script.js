1cf51b073dc6500d428df84618b790180e7421f9
CelsonF <celson338540@gmail.com>
2021-08-26 00:12:21 -0300

    Add function definecolors and concludes Counter

--- Counter/js/script.js
+++ Counter/js/script.js
@@ -1,3 +1,4 @@
+try {
 //set initial count
 let count = 0;
 
@@ -5,8 +6,37 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
+function defineColors(count) {
+ if (count > 0) {
+     value.style.color = 'green'
+ }
+ if (count < 0) {
+     value.style.color = 'red'
+ }
+ if (count === 0) {
+     value.style.color = '#222'
+ }
+}
+
 btns.forEach(function (btn) {
     btn.addEventListener('click', function(e) {
-        console.log(e.currentTarget.classList);
+        const styles = e.currentTarget.classList;
+
+        if(styles.contains('decrease')) {
+          count--;
+        }
+        else if(styles.contains('increase')){
+           count++;
+        }
+        else {
+            count = 0;
+        }
+        defineColors(count);
+
+        value.textContent = count;
+        
     });
 });
+} catch (error) {
+    console.log(`${error} : Verifique a função dos botões. `)
+}
